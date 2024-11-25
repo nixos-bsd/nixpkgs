e813044a1ae02f96e76a31a5c8c417ab2e08394c
Artemis Tosini <me@artem.ist>
2024-11-25 23:30:14 +0000

    openrc: Support build for FreeBSD

--- pkgs/by-name/op/openrc/package.nix
+++ pkgs/by-name/op/openrc/package.nix
@@ -2,7 +2,6 @@
   lib,
   stdenv,
   fetchFromGitHub,
-  fetchpatch,
   meson,
   cmake,
   ninja,
@@ -11,6 +10,7 @@
   linux-pam,
   libcap,
   libselinux,
+  freebsd,
 }:
 
 stdenv.mkDerivation rec {
@@ -38,10 +38,27 @@ stdenv.mkDerivation rec {
   # DESTDIR is required for some reason.
   DESTDIR = "/";
 
-  # Normally meson_runlevels.sh would try to do this
+  # Normally meson_runlevels.sh would try to do this,
+  # it's the only thing we want out of that script
   postInstall = ''
     ln -s $out/lib/rc/sh/functions.sh $out/etc/init.d/functions.sh
   '';
+
+  mesonFlags =
+    lib.optionals stdenv.isFreeBSD [
+      "-Dos=FreeBSD"
+      "-Daudit=disabled"
+      "-Dselinux=disabled"
+      "-Dcapabilities=disabled"
+    ]
+    ++ lib.optionals stdenv.isLinux [
+      "-Dos=Linux"
+    ];
+
+  env = lib.optionalAttrs stdenv.isFreeBSD {
+    NIX_CFLAGS_COMPILE = "-I${freebsd.include}/include -I${freebsd.libpam}/include";
+    NIX_CFLAGS_LINK = "-lkvm";
+  };
 
   nativeBuildInputs = [
     meson
@@ -50,12 +67,16 @@ stdenv.mkDerivation rec {
     pkg-config
   ];
 
-  buildInputs = lib.optionals stdenv.isLinux [
-    audit
-    linux-pam
-    libcap
-    libselinux
-  ];
+  buildInputs =
+    lib.optionals stdenv.isLinux [
+      audit
+      linux-pam
+      libcap
+      libselinux
+    ]
+    ++ lib.optionals stdenv.isFreeBSD [
+      freebsd.libpam
+    ];
 
   meta = with lib; {
     description = "OpenRC init system";
